93c0150ab610da981d87e24b3b2ae42b20fc49b3
Allan de Miranda Silva <allandemiranda@gmail.com>
2021-03-31 20:38:16 -0300

    new env

--- exemplo.py
+++ exemplo.py
@@ -1,7 +1,14 @@
 import logging
 from iqoptionapi.stable_api import IQ_Option
 
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
-I_want_money = IQ_Option("email", "senha")
+I_want_money = IQ_Option(os.environ.get("LOGIN"), os.environ.get("PASSWORD"))
 check, reason = I_want_money.connect()  # connect to iqoption
 print(check, reason)
